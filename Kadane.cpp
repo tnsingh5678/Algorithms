bba8653e1a6bb6912b945514c66ab106b950edb4
tripurarinathsingh <tripurarinathsingh50@gmail.com>
2025-01-08 18:51:10 +0530

    Trie

--- Kadane.cpp
+++ Kadane.cpp
@@ -1,28 +1,32 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-class Solution{
+class Solution
+{
 public:
-    int Kadane(vector<int>& nums){
+    int Kadane(vector<int> &nums)
+    {
         int ans = INT_MIN;
-        int a=0;
-        for(int num : nums){
-            a+=num;
-            ans=max(ans,a);
-            a=max(a,0);
+        int a = 0;
+        for (int num : nums)
+        {
+            a += num;
+            ans = max(ans, a);
+            a = max(a, 0);
         }
         return ans;
     }
 };
- 
-int main(){
+
+int main()
+{
     Solution obj;
     int n;
-    cin>>n;
+    cin >> n;
     vector<int> v(n);
-    for(int i=0;i<n;i++){
-        cin>>v[i];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
     }
-    cout<<obj.Kadane(v)<<endl;
-
+    cout << obj.Kadane(v) << endl;
 }
---
+++ Trie.cpp
@@ -0,0 +1,64 @@
+#include<bits/stdc++.h>
+using namespace std;
+class TrieNode{
+    public:
+    unordered_map<char,TrieNode*> children;
+    bool isEndOfWord;
+
+    TrieNode(){
+        isEndOfWord = false;
+    }
+
+};
+class Trie{
+    public:
+
+    TrieNode* root;
+
+    Trie(){
+        root = new TrieNode;
+    }
+
+    void insert(const string& word){
+        TrieNode* node = root;
+        for(auto ch : word){
+            if(node->children.find(ch)==node->children.end()){
+                node->children[ch] = new TrieNode();
+            }
+            node = node->children[ch];
+        }
+        node->isEndOfWord = true;
+    }
+
+    bool search(const string& word){
+        TrieNode* node = root;
+        for(auto ch : word){
+            if(node->children.find(ch)==node->children.end()){
+                return false;
+            }
+            node = node->children[ch];
+        }
+        return node->isEndOfWord;
+    }
+
+    bool startsWith(const string& word){
+        TrieNode* node = root;
+        for(auto ch : word){
+            if(node->children.find(ch)==node->children.end()){
+                return false;
+            }
+            node = node->children[ch];
+        }
+        return true;
+    }
+
+};
+int main(){
+    Trie t;
+    t.insert("hello");
+    t.insert("help");
+    cout<<t.search("hello")<<endl;
+    cout<<t.startsWith("hel")<<endl;
+    cout<<t.search("hel")<<endl;
+
+}
